7391d42e633f7520bd581ff1ed0a971f4872dc78
Lucas <llucass_souza@hotmail.com>
2026-07-28 11:33:46 -0300

    feat(docker): add storefront containerization and production nginx routing

--- deploy/init-letsencrypt.sh
+++ deploy/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.friggafrio.com.br)
+domains=(friggafrio.com.br www.friggafrio.com.br api.friggafrio.com.br admin.friggafrio.com.br)
 rsa_key_size=4096
 data_path="./certbot"
 email="contato@friggafrio.com.br" # Substitua pelo seu email real
